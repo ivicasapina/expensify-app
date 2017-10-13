1c1af306a4a1aaff967c1aeffa965c7192037dcc
Ivica Šapina <ivica.moke@gmail.com>
2017-10-13 10:50:59 +0200

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -19,9 +19,28 @@ const jsx = (
   </Provider>
 );
 
+let hasRenedred = false;
+const renderApp = () => {
+  if (!hasRenedred) {
+    ReactDOM.render(jsx, document.querySelector('#app'));
+    hasRenedred = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.querySelector('#app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.querySelector('#app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
-
