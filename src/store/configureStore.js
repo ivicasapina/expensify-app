e63a1063342a88ab42b477f6d6b2a0bb7229e293
Ivica Šapina <ivica.moke@gmail.com>
2018-03-22 10:47:21 +0100

    improved redux devTool hide

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,7 @@ import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
-let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-if (process.env.NODE_ENV === 'production') {
-  composeEnhancers = compose;
-}
+let composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 // Combine Reducers
 const rootReducer = combineReducers({
