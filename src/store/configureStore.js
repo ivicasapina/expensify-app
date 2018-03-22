e2f8f32f987bd38790ea13a1935aba35f789d8e6
Ivica Šapina <ivica.moke@gmail.com>
2018-03-22 10:41:09 +0100

    removing redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,11 @@ import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+if (process.env.NODE_ENV === 'production') {
+  composeEnhancers = compose;
+}
 
 // Combine Reducers
 const rootReducer = combineReducers({
