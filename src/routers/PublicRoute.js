af58c171a0dbda3fec1d45cfbecf5de0dd358718
Ivica Šapina <ivica.sapina@domnet.ba>
2019-04-08 16:49:22 +0200

    Public route commented

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
+// PublicRoute commented
 export const PublicRoute = ({
   isAuthenticated,
   component: Component,
   ...rest
-  }) => (
+}) => (
     <Route {...rest} component={(props) => (
       isAuthenticated
         ? (<Redirect to="/dashboard" />)
