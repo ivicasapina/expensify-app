f7af01c210a5e80e99e4627d2e21bba8283d3949
Ivica Šapina <ivica.sapina@domnet.ba>
2019-04-08 16:57:54 +0200

    commit-1

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
+//asdas
 export const PublicRoute = ({
   isAuthenticated,
   component: Component,
